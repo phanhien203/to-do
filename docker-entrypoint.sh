9b8b6e032ea1c1f370da7a3e215cba23ca78cd46
hienphan <hien.phan@kamora.vn>
2025-09-02 23:02:38 +0700

    +

---
+++ docker-entrypoint.sh
@@ -0,0 +1,8 @@
+set -ex
+
+find /usr/share/nginx/html -type f \( -name "*.js" -o -name "*.html" \) | while read file; do
+    sed -i "s|%BASE_URL%|$BASE_URL|g" "$file"
+    envsubst '$BASE_URL' < "$file" > "$file.tmp"
+done
+
+exec "$@"
